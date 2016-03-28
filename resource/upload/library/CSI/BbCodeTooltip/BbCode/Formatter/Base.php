a1cdf0c610b68337d5966fc7c2ecd66f969e3954
Kitsune Solar <kitsune.solar@gmail.com>
2016-03-28 14:51:28 +0300

    Init resource

---
+++ resource/upload/library/CSI/BbCodeTooltip/BbCode/Formatter/Base.php
@@ -0,0 +1,43 @@
+<?php namespace CSI\BbCodeTooltip\BbCode\Formatter;
+
+/**
+ * Class Base
+ * @package CSI\BbCodeTooltip\BbCode\Formatter
+ */
+class Base
+{
+  /**
+   * @param array $tag
+   * @param array $rendererStates
+   * @param \XenForo_BbCode_Formatter_Base $formatter
+   * @return mixed
+   */
+  public static function getBbCodeTooltip(array $tag, array $rendererStates, \XenForo_BbCode_Formatter_Base $formatter)
+  {
+    $xenOptions = \XenForo_Application::get('options');
+    $xenVisitor = \XenForo_Visitor::getInstance();
+    $tagOption = array_map('trim', explode('|', $tag['option']));
+
+    if (count($tagOption) > 1) {
+      $optDefault = $tagOption[0];
+    } else {
+      $optDefault = $tag['option'];
+    }
+
+    $tagContent = $formatter->renderSubTree($tag['children'], $rendererStates);
+    $view = $formatter->getView();
+
+    if ($view) {
+      $template = $view->createTemplateObject('csiXF_bbCode_964E60C3_tag_tooltip',
+        array(
+          'content' => $tagContent,
+          'option' => $optDefault,
+        ));
+
+      $tagContent = $template->render();
+      return trim($tagContent);
+    }
+
+    return $tagContent;
+  }
+}
